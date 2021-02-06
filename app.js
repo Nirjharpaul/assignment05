3cb01bb90cd4568f8e1b025ceeb1c37acd261b2a
nirjhar paul <nirjhar029@gmail.com>
2021-02-07 01:52:28 +0600

    commit

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-
-
-
 document.getElementById('search-button').addEventListener('click', function () {
     const mealInput = document.getElementById('meal-input').value;
     fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
         .then(res => res.json())
-        .then(data => displayMeal(data))
+        .then(data => console.log(data))
         .catch(err => alert('Not Available This Meal'))
 })
 
 
 
-const displayMeal = food => {
+const displayMeal = meals => {
 
+
+    // const foodsDiv = document.getElementById('meal-container')
+    // for (let i = 0; i < meals.length; i++) {
+    //     const foodItem = meals.categories[0];
+    //     const foodDiv = document.createElement('div');
+    //     foodDiv.innerText = foodItem.categories;
+    //     foodsDiv.appendChild(foodDiv)
+    // }
 }
